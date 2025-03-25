c9d6f3b4e0e9772455c0e6a5b529e1a9d003185b
DevanshuKansal03 <devanshu03@antiersolutions.com>
2025-03-25 18:27:56 +0530

    Add

---
+++ Concept.js
@@ -0,0 +1,6 @@
+// 1- Node.js is not a programming Language, it is a runtime environment that allows JavaScript to run outside the browser. It is built on the V8 JavaScript engine 
+// 2- Browser like the chrome etc have in built engines which processes and renders the html, css and js to display web pages
+// 3- In simple words a Nodejs is just like a container or like an environment in which our js code can be run outside the browser
+// 4- If we use it outside the browser then we can access the data files of the system and can interact with the database like mongodb, mysql without needed of the browser api's
+// 5- We will not prefer nodejs if we are doing the heavy server side processing like- image manipulation, video conversions and file compressions
+// 6- Node.js is a single threaded based on event driven, non blocking I/O Model
